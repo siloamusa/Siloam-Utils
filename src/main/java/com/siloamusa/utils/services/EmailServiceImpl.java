4df224a57b6aec78cdaf5326339ef3bf311fc4fc
Siloam USA <siloam.usa.llc@gmail.com>
2026-02-19 17:11:49 -0500

    Log Debug

--- src/main/java/com/siloamusa/utils/services/EmailServiceImpl.java
+++ src/main/java/com/siloamusa/utils/services/EmailServiceImpl.java
@@ -2,10 +2,15 @@
 package com.siloamusa.utils.services;
 
 
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
 import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -18,6 +23,27 @@ public class EmailServiceImpl implements EmailService {
 
     @Autowired 
     private JavaMailSender javaMailSender;
+
+    @PostConstruct
+    public void initDebug() {
+        if (javaMailSender instanceof JavaMailSenderImpl mailSender) {
+            // Force the JavaMail Session to use SLF4J
+            mailSender.getSession().setDebug(true);
+            mailSender.getSession().setDebugOut(new PrintStream(new OutputStream() {
+                private final StringBuilder line = new StringBuilder();
+                @Override
+                public void write(int b) {
+                    if (b == '\n') {
+                        log.info("[JavaMail] {}", line.toString().trim());
+                        line.setLength(0);
+                    } else if (b != '\r') {
+                        line.append((char) b);
+                    }
+                }
+            }));
+            log.info("JavaMail Debug routing initialized.");
+        }
+    }
 
     @Async("emailExecutor") // Reference the bean name from your config
     public void sendEmailAsync(EmailDetails details) {
